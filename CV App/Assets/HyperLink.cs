0701efb5ce4d6d0cf7d47bbf79d277c1770941b4
alexwills1 <promoclimate@gmail.com>
2021-05-08 19:30:42 +1200

    Total CV App
    Made my first apple app in a couple days for iphone 11.

---
+++ CV App/Assets/HyperLink.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperLink : MonoBehaviour
+{
+    public void OpenGmailLink()
+    {
+        Application.OpenURL ("mailto:alex.wills.nz@gmail.com");
+    }
+
+    public void OpenLinkedinLink()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/alex-wills-2184a115b/");
+    }
+
+    public void callPhone()
+    {
+        Application.OpenURL("tel://0278418971");
+    }
+
+    public void uiVideo()
+    {
+        Application.OpenURL("https://youtu.be/h-GEU3_8uLM");
+    }
+
+    public void trailerVideo()
+    {
+        Application.OpenURL("https://youtu.be/S9bIcWhdwWg");
+    }
+}
---
+++ CV App/Assets/UIHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour{
+
+    public GameObject ScrollCV;
+    public GameObject Home;
+
+   public void buttonEvent()
+    {
+            ScrollCV.SetActive(true);
+    }
+
+    public void buttonEvent2()
+    {
+            Home.SetActive(true);
+    }
+}
